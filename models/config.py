fd698be3504646d7308960a04e8a3a1fd71b3127
josevsousa <josev@josev.com.br>
2016-04-04 14:03:27 -0300

    funcao format cnpj_e_cpf de volta no model

--- models/config.py
+++ models/config.py
@@ -77,3 +77,18 @@ class MODELO_VALIDACAO(object):
         return formatado
 #fim do valida cpf
      
+class cnpj_cpf_formt(object):
+
+    def __init__(self, valor):
+        self.valor = valor          
+
+    def t(self):
+        value = self.valor
+        if len(value)==11:
+            formatado = value[0:3]+'.'+value[3:6]+'.'+value[6:9]+'-'+value[9:11]
+        elif len(value)==14:
+            formatado = value[0:2]+'.'+value[2:5]+'.'+value[5:8]+'/'+value[8:12]+'-'+value[12:14]
+        else:
+            formatado = 'valor invalido!'
+        #formatado = value
+        return formatado  
