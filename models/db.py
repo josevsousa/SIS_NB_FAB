0e39030468a295ecfdf7072ad52adbf2e50380d0
josevsousa <josev@josev.com.br>
2016-04-17 11:48:18 -0300

    tempo da session em 10 dias

--- models/db.py
+++ models/db.py
@@ -320,3 +320,5 @@ Produtos_config = db.define_table('produtos_config',
 # Caixa_Cliente.representante.requires = IS_IN_DB(db, Representante.nome, error_message = 'Usuário invalido') 
 # Caixa_Cliente.nome.widget = SQLFORM.widgets.autocomplete(
 # request, db.clientes.nome, limitby=(0,10), min_length=2)
+
+auth.settings.expiration = 864000 #10 dias de sessao aberta 
